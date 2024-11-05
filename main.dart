adc582766a0021e6e4b36f25a4785d10d22d20c1
Bayumodesenyap <bayuuye890@gmail.com>
2024-11-05 14:55:01 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -8,27 +8,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // Root widget
+    return MaterialApp(
+      // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Home Page'),
+          title: const Text('LOGIN PAGE'),
+          
         ),
         body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Click!');
-                    },
-                    child: const Text('A button'),
-                  ),
-                ],
-              );
-            },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 200),
+            child: Builder(
+              builder: (context) {
+                
+                return Column(
+                  children: [
+                    const Text(
+                      'flutter',
+                      style: TextStyle(fontSize: 65),
+                      
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),const SizedBox(height: 20),
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(height: 46),
+                    const Text(style: TextStyle(color: Colors.blueAccent),"forgot password ?"),
+                    
+                    const SizedBox(height: 8.0,),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 150, 1, 155)),
+                      onPressed: () {
+                        print('Click!');
+                      },
+                      child: const Text('LOGIN'),
+                    ),
+                  ],
+                );
+                
+              },
+            ),
           ),
         ),
       ),
