b8b62db9fcd65bc7b277b75231f137fc72f2032d
Bayumodesenyap <bayuuye890@gmail.com>
2024-11-12 11:48:13 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,67 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('LOGIN PAGE'),
-          
+          title: Text('Container Widget Example'),
         ),
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 200),
-            child: Builder(
-              builder: (context) {
-                
-                return Column(
-                  children: [
-                    const Text(
-                      'flutter',
-                      style: TextStyle(fontSize: 65),
-                      
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                      ),
-                    ),const SizedBox(height: 20),
-                    TextField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(height: 46),
-                    const Text(style: TextStyle(color: Colors.blueAccent),"forgot password ?"),
-                    
-                    const SizedBox(height: 8.0,),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 150, 1, 155)),
-                      onPressed: () {
-                        print('Click!');
-                      },
-                      child: const Text('LOGIN'),
-                    ),
-                  ],
-                );
-                
-              },
-            ),
+          child: Container(
+          height: 220,
+          width: double.infinity,
+          margin: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+             gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 255, 204),
+                Color.fromARGB(255, 2, 250, 27),
+                Color.fromARGB(255, 255, 247, 0),
+                Color.fromARGB(255, 255, 0, 0)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+             )
           ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title: Flutter Stuff',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const SizedBox(height: 8),
+              const Divider(color: Colors.black),
+              const Text(
+                'Description: This is a card-like layout using a Container widget. It has padding, margin, and a box shadow.',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('visit to read more',
+                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),))))
+            ],
+          ),
+        ),
         ),
       ),
     );
+    
   }
 }
